0950b663ec0fe11645f4dc157a98f05d0370ecf1
nathan <nathan.gould@gmail.com>
2015-05-02 16:00:20 -0400

    adding blank content script

--- chrome/src/bg/background.js
+++ chrome/src/bg/background.js
@@ -4,10 +4,30 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
+console.log("Yeah");
 
 //example of using a message handler from the inject scripts
+/*
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+*/
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    if(request.cmd == "read_file") {
+        $.ajax({
+            url: chrome.extension.getURL("topbar.html"),
+            dataType: "html",
+            success: sendResponse
+        });
+    }
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+	  console.log('Turning ' + tab.url + ' red!');
+	  chrome.tabs.executeScript({
+	    code: 'document.body.style.backgroundColor="red"'
+    });
+});
