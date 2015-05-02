d87b45e5096d2e5fe7019b1eb61c1b9ab64c664c
nathan <nathan.gould@gmail.com>
2015-05-02 15:00:43 -0400

    adding dan's box stuff

--- chrome/src/browser_action/browser_action.js
+++ chrome/src/browser_action/browser_action.js
@@ -1,14 +1,11 @@
 Firebase.enableLogging(true);
-var f = new Firebase('https://chrome-sample.firebaseio-demo.com/');
 
-f.transaction(function(curr) {
-  if (isNaN(parseFloat(curr)))
-    return 1; // initialize to 1.
-  else
-    return curr + 1; // increment.
-}, function() {
-    // Once the transaction has completed, update the UI (and watch for updates).
-    f.on('value', function(s) {
-      document.getElementById('contents').innerHTML = s.val();
-    });
-  });
+var myFirebaseRef = new Firebase("https://glaring-fire-3108.firebaseio.com/");
+
+usersRef = myFirebaseRef.child('users')
+
+usersRef.push({
+	email: "nathan.gould@gmail.com",
+	first_name: "Nathan",
+	last_name: "Gould"
+});
