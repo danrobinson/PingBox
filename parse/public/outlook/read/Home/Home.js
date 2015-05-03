4a6c27784fd9273443a53204a2b13e6352530e33
dgrtwo <dgrtwo@princeton.edu>
2015-05-03 07:42:58 -0400

    Outlook improvements

--- parse/public/outlook/read/Home/Home.js
+++ parse/public/outlook/read/Home/Home.js
@@ -14,6 +14,17 @@
 
     // Displays the "Subject" and "From" fields, based on the current mail item
     function displayItemDetails() {
+        Parse.initialize("8fhsO5d7WTt6c7ffpVrPpHTVvuAi6vArrciyt8cK", 
+                         "1GHMsEbKTKr7ZhLqcJUPcOJdi7CLD1YZeT4hGuEv");
+
+        if (Parse.User.current() === null) {
+          $("#content-main").hide();
+          $("#content-login").show();
+        } else {
+          $("#content-main").show();
+          $("#content-login").hide();
+        }
+
         var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
 
         // Set task's title to the email's subject
@@ -40,7 +51,7 @@
             var cc = Office.context.mailbox.item.cc;
             for (var i=0; i < cc.length; i++) {
                 watchlist = watchlist.concat(cc[i].emailAddress);
-                $('#taskassigner').val(watchlist.concat(cc[0].emailAddress));
+                $('#taskassignedto').val(watchlist.concat(cc[0].emailAddress));
                 // $('#taskwatchers').concat('<input type="email" class="form-control" value="'+cc[i].emailAddress+'">');
             }
         } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
@@ -57,9 +68,55 @@
         } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
             // TODO(cjr) add support for appointments
         }
-
+        
         var watchEmails = watchlist.join(', ');
         $('#taskwatch').val(watchEmails);
+        
+        $("#submit-button").click(function() {
+          // TODO: Logic for adding task
+          var title = $('#tasktitle').val();
+          var assignee = $('#taskassignedto').val();
+          var description = $('#taskdesc').val();
+          var watchers = $('#taskwatch').val().replace(/ /g, '').split(",");      
+          var email = null;  // TODO: set up link to email
+          
+          write(title + " " + assignee);
+          
+          Parse.Cloud.run("assignTask", {
+            title: title,
+            description: description,
+            assignee: assignee,
+            watchers: watchers,
+            email: email,
+          }, {
+            success: function(result) {
+              $("#assign-success").html("Task created!");
+            },
+            error: function(error) {
+              $("#assign-alert").html("error " + error.message);
+            }
+          })
+        });
+        $("#login").submit(function(event) {
+          event.preventDefault();
+        
+          var username = $("#username").val();
+          var password = $("#password").val();
 
+          Parse.User.logIn(username, password, {
+            success: function(user) {
+              displayItemDetails();
+            },
+            error: function(user, error) {
+              $("#login-alert").html("Error: " + error.message);
+            }
+          });
+        });
+        $("#logout-button").click(function() {
+          Parse.User.logOut();
+          displayItemDetails();
+        });
+                
+        write("console 8");
     }
 })();
